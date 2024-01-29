e77332e45eaac92d1a7391ed8245c02b2d808ceb
Justin <justin.ma@live.ca>
2024-01-29 13:19:43 -0500

    continued to refactor files, may need to hard restart

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage';
 import './App.scss';
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route />
+          <Route path='/' to={HomePage} />
         </Routes>
       </BrowserRouter>
     </>
