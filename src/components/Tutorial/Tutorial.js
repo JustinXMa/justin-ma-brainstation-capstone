b9660a2cf6e73ce5a8f1204bb9974cf2eef78dfd
Justin <justin.ma@live.ca>
2024-01-30 02:48:55 -0500

    updated styles and checked for possible issues with mapped out tutorial list items by hard coding temporary items

--- src/components/Tutorial/Tutorial.js
+++ src/components/Tutorial/Tutorial.js
@@ -15,8 +15,74 @@ export default function Tutorial() {
                     </div>
                     <p className="tutorial__category">TEMP CATEGORY</p>
                 </div>
-                <p className="tutorial__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum pariatur delectus, error sequi eum quisquam excepturi dolorem impedit esse molestias iste ipsa doloribus inventore! Ipsa odit deserunt quos maiores aut!</p>
-                <img className='tutorial__preview' src={TempImg} alt="" />
+                <p className="tutorial__description">TEMP Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum pariatur delectus, error sequi eum quisquam excepturi dolorem impedit esse molestias iste ipsa doloribus inventore! Ipsa odit deserunt quos maiores aut!</p>
+                <img className='tutorial__preview' src={TempImg} alt="TEMP TITLE" />
+                <div className="tutorial__icon-container">
+                    <div className="tutorial__views-container">
+                        <img src={EnderEye} alt="" className="tutorial__icon" />
+                        <span className="tutorial__views">TEMP 3</span>
+                    </div>
+                    <div className="tutorial__likes-container">
+                        <img src={MinecraftHeart} alt="" className="tutorial__icon--heart" />
+                        <span className="tutorial__likes">TEMP 4</span>
+                    </div>
+                </div>
+            </div>
+            <hr className="tutorial__divider" />
+            <div className="tutorial__main-container">
+                <div className="tutorial__title-and-category">
+                    <div className="tutorial__title-and-creator">
+                        <h2 className="tutorial__title">TEMP TITLE</h2>
+                        <p className="tutorial__creator">By: TEMP AUTHOR</p>
+                    </div>
+                    <p className="tutorial__category">TEMP CATEGORY</p>
+                </div>
+                <p className="tutorial__description">TEMP Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum pariatur delectus, error sequi eum quisquam excepturi dolorem impedit esse molestias iste ipsa doloribus inventore! Ipsa odit deserunt quos maiores aut!</p>
+                <img className='tutorial__preview' src={TempImg} alt="TEMP TITLE" />
+                <div className="tutorial__icon-container">
+                    <div className="tutorial__views-container">
+                        <img src={EnderEye} alt="" className="tutorial__icon" />
+                        <span className="tutorial__views">TEMP 3</span>
+                    </div>
+                    <div className="tutorial__likes-container">
+                        <img src={MinecraftHeart} alt="" className="tutorial__icon--heart" />
+                        <span className="tutorial__likes">TEMP 4</span>
+                    </div>
+                </div>
+            </div>
+            <hr className="tutorial__divider" />
+            <div className="tutorial__main-container">
+                <div className="tutorial__title-and-category">
+                    <div className="tutorial__title-and-creator">
+                        <h2 className="tutorial__title">TEMP TITLE</h2>
+                        <p className="tutorial__creator">By: TEMP AUTHOR</p>
+                    </div>
+                    <p className="tutorial__category">TEMP CATEGORY</p>
+                </div>
+                <p className="tutorial__description">TEMP Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum pariatur delectus, error sequi eum quisquam excepturi dolorem impedit esse molestias iste ipsa doloribus inventore! Ipsa odit deserunt quos maiores aut!</p>
+                <img className='tutorial__preview' src={TempImg} alt="TEMP TITLE" />
+                <div className="tutorial__icon-container">
+                    <div className="tutorial__views-container">
+                        <img src={EnderEye} alt="" className="tutorial__icon" />
+                        <span className="tutorial__views">TEMP 3</span>
+                    </div>
+                    <div className="tutorial__likes-container">
+                        <img src={MinecraftHeart} alt="" className="tutorial__icon--heart" />
+                        <span className="tutorial__likes">TEMP 4</span>
+                    </div>
+                </div>
+            </div>
+            <hr className="tutorial__divider" />
+            <div className="tutorial__main-container">
+                <div className="tutorial__title-and-category">
+                    <div className="tutorial__title-and-creator">
+                        <h2 className="tutorial__title">TEMP TITLE</h2>
+                        <p className="tutorial__creator">By: TEMP AUTHOR</p>
+                    </div>
+                    <p className="tutorial__category">TEMP CATEGORY</p>
+                </div>
+                <p className="tutorial__description">TEMP Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum pariatur delectus, error sequi eum quisquam excepturi dolorem impedit esse molestias iste ipsa doloribus inventore! Ipsa odit deserunt quos maiores aut!</p>
+                <img className='tutorial__preview' src={TempImg} alt="TEMP TITLE" />
                 <div className="tutorial__icon-container">
                     <div className="tutorial__views-container">
                         <img src={EnderEye} alt="" className="tutorial__icon" />
