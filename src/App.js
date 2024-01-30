12e6f27e9db51796969e7137f5d23ad2030c3567
Justin <justin.ma@live.ca>
2024-01-30 01:14:56 -0500

    first draft of tutorial list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage';
 import SignUp from './pages/SignUp/SignUp';
 import './App.scss';
+import TutorialList from './pages/TutorialList/TutorialList';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path='/' element={<HomePage />} />
           <Route path='/login' element={<HomePage />} />
           <Route path='/signup' element={<SignUp />} />
+          <Route path='/list' element={<TutorialList />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/TutorialList/TutorialList.js
@@ -0,0 +1,25 @@
+import './TutorialList.scss';
+
+export default function TutorialList() {
+    return (
+        <main className="tutorial-list">
+            <div className="tutorial-list__banner">
+                <h1 className="tutorial-list__title"></h1>
+            </div>
+            <div className="tutorial-list__nav">
+                <div className="tutorial-list__name-and-category">
+                    <h2 className="tutorial-list__category">Build Name</h2>
+                    <h2 className="tutorial-list__category">Category</h2>
+                </div>
+                <h2 className="tutorial-list__category">Description</h2>
+                <h2 className="tutorial-list__category">Preview</h2>
+                <div className="tutorial-list__views-and-likes">
+                    <h2 className="tutorial-list__category">Views</h2>
+                    <h2 className="tutorial-list__category">Likes</h2>
+                </div>
+            </div>
+            <div className="tutorial-list__item">
+            </div>
+        </main>
+    )
+}
