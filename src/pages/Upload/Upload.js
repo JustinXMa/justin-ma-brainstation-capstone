47fa3ac41e437bc0bf4cad387ce6e8ea23b4c1a8
Justin <justin.ma@live.ca>
2024-01-30 10:17:08 -0500

    created first draft of upload page

---
+++ src/pages/Upload/Upload.js
@@ -0,0 +1,10 @@
+import './Upload.scss';
+
+export default function Upload() {
+    return (
+        <main className="upload">
+            <h2 className="upload__title">New Build</h2>
+
+        </main>
+    )
+}
