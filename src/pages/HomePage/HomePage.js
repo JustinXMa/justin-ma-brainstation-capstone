96d8ef657a34bab13bb555881db3ea5619859435
Justin <justin.ma@live.ca>
2024-02-01 00:23:47 -0500

    added continue as guest, added styles for new additions, added new background

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -9,11 +9,14 @@ export default function HomePage() {
             </div>
             <div className="home-page__input-container">
                 <label htmlFor="user" className="home-page__user"></label>
-                <input type="text" className="home-page__user-input" id='user' name='user' placeholder='Username' />
+                <input type="text" className="home-page__user-input" id='user' name='user' placeholder='Username or Email' />
                 <label htmlFor="password" className="home-page__password"></label>
                 <input type="password" className="home-page__password-input" id='password' name='password' placeholder='Password' />
                 <button className="home-page__button">Login</button>
-                <p className="home-page__sign-up">New here? <Link to='/signup'><span className="home-page__sign-up-link">Register</span></Link></p>
+                <div className="home-page__alt-login-options-container">
+                    <p className="home-page__sign-up">New here? <Link to='/signup' className="home-page__sign-up--link">Register</Link></p>
+                    <p className='home-page__guest'>Continue as <Link to='/list' className='home-page__guest--link'>Guest</Link></p>
+                </div>
             </div>
         </main>
     )
