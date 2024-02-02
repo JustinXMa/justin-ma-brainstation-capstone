ffa4e98fee4f32f924b6871bf023b4f3e5f2377a
Justin <justin.ma@live.ca>
2024-02-02 12:48:39 -0500

    attempted to make a confirm sign up page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage';
 import SignUp from './pages/SignUp/SignUp';
+import SignUpSuccess from './components/SignUpSuccess/SignUpSuccess';
 import TutorialList from './pages/TutorialList/TutorialList';
 import Upload from './pages/Upload/Upload'
 import TutorialDetails from './pages/TutorialDetails/TutorialDetails';
@@ -14,9 +15,11 @@ function App() {
           <Route path='/' element={<HomePage />} />
           <Route path='/login' element={<HomePage />} />
           <Route path='/signup' element={<SignUp />} />
+          <Route path='/redirecting' element={<SignUpSuccess />} />
           <Route path='/list' element={<TutorialList />} />
           <Route path='/upload' element={<Upload />} />
           <Route path='/tutorial/:id' element={<TutorialDetails />} />
+          <Route path='*' elemnt={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/components/SignUpSuccess/SignUpSuccess.js
@@ -0,0 +1,7 @@
+import './SignUpSuccess.scss'
+
+export default function SignUpSuccess() {
+    <section className="sign-up-success">
+        <h2 className="sign-up-success__title">Loading...</h2>
+    </section>
+}
