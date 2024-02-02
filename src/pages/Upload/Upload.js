afd48d74ed760d5099c36d1b8473fbdaa94518b9
Justin <justin.ma@live.ca>
2024-02-02 05:34:24 -0500

    removed unused state variables, added return false statements in lieu of removed variables

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -11,28 +11,20 @@ export default function Upload() {
     const [instructions, setInstructions] = useState('')
     const [preview, setPreview] = useState(null);
 
-    const [errorBuildName, setErrorBuildName] = useState('')
-    const [errorCategory, setErrorCategory] = useState('')
-    const [errorDescription, setErrorDescription] = useState('')
-    const [errorInstructions, setErrorInstructions] = useState('')
 
     const navigate = useNavigate();
 
     const changeBuildName = (event) => {
         setBuildName(event.target.value)
-        console.log('title:', buildName)
     }
     const changeCategory = (event) => {
         setCategory(event.target.value)
-        console.log('category: ', category)
     }
     const changeDescription = (event) => {
         setDescription(event.target.value)
-        console.log('description: ', description)
     }
     const changeInstructions = (event) => {
         setInstructions(event.target.value)
-        console.log('instructions: ', instructions)
     }
 
     const handleFileChange = (event) => {
@@ -42,25 +34,25 @@ export default function Upload() {
 
     const isBuildNameValid = () => {
         if (buildName.length === 1) {
-            setErrorBuildName('Please title your tutorial');
+            return false;
         }
         return true;
     }
     const isCategoryValid = () => {
         if (category.length === 1) {
-            setErrorCategory('Please select a category');
+            return false;
         }
         return true;
     }
     const isDescriptionValid = () => {
         if (description.length === 1) {
-            setErrorDescription('Please add a description');
+            return false;
         }
         return true;
     }
     const isInstructionsValid = () => {
         if (instructions.length === 1) {
-            setErrorInstructions('Please add instructions');
+            return false;
         }
         return true;
     }
@@ -128,29 +120,26 @@ export default function Upload() {
                     <div className="upload__input-container">
                         <label htmlFor="build" className="upload__build">Build Name:</label>
                         <input onChange={changeBuildName} value={buildName} type="text" className="upload__build-input" id='build' name='build' />
-                        {errorBuildName && <p className="upload__error">{errorBuildName}</p>}
                     </div>
                     <div className="upload__input-container">
                         <label htmlFor="category" className="upload__category">Category:</label>
                         <div className="upload__dropdown-container">
                             <select onChange={changeCategory} value={category} name="category" id="category" className="upload__dropdown-menu">
-                                <option value="">--Please Select--</option>
+                                <option value="" disabled
+                                >--Please Select--</option>
                                 <option value="Architecture">Architecture</option>
                                 <option value="RedStone">RedStone</option>
                                 <option value="Nature">Nature</option>
                             </select>
-                            {errorCategory && <p className="upload__error">{errorCategory}</p>}
                         </div>
                     </div>
                     <div className="upload__input-container">
                         <label htmlFor="description" className="upload__description">Description:</label>
                         <textarea onChange={changeDescription} value={description} name="description" id="description" cols="30" rows="10" className="upload__description-input"></textarea>
-                        {errorDescription && <p className="upload__error">{errorDescription}</p>}
                     </div>
                     <div className="upload__input-container">
                         <label htmlFor="instructions" className="upload__instructions">Instructions:</label>
                         <textarea onChange={changeInstructions} value={instructions} name="instructions" id="instructions" cols="30" rows="10" className="upload__instructions-input"></textarea>
-                        {errorInstructions && <p className="upload__error">{errorInstructions}</p>}
                     </div>
                 </div>
                 <div className="upload__image-and-button-container">
