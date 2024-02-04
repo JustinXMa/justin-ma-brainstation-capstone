4bbb41c6c89872ec9ecfbe805a36af655a4432d8
Justin <justin.ma@live.ca>
2024-02-04 14:48:06 -0500

    created sign up successful page

--- src/components/SignUpSuccess/SignUpSuccess.js
+++ src/components/SignUpSuccess/SignUpSuccess.js
@@ -1,7 +1,20 @@
+import { useNavigate } from 'react-router-dom';
 import './SignUpSuccess.scss'
 
 export default function SignUpSuccess() {
-    <section className="sign-up-success">
-        <h2 className="sign-up-success__title">Loading...</h2>
-    </section>
+    const navigate = useNavigate();
+    const delay = 2000;
+    setTimeout(() => {
+        navigate('/login');
+    }, delay);
+    return (
+        <section className="sign-up-success">
+            <h2 className="sign-up-success__title">Sign-up Successful!</h2>
+            <div className="sign-up-success__animation-container">
+                <div className="sign-up-success__border">
+                    <div className="sign-up-success__progress"></div>
+                </div>
+            </div>
+        </section>
+    )
 }
